bd4ad7ac1f7996ece9b3b8cf6b642d47c84d1650
Venkatesh Nichenametla <vni6kor@bosch.com>
2021-02-25 20:37:26 +0530

    Corrected as per the review findings

--- TelCo.ColorCoder/ColorCodeIdentifierTest.cs
+++ TelCo.ColorCoder/ColorCodeIdentifierTest.cs
@@ -3,38 +3,48 @@ using System.Diagnostics;
 using System.Drawing;
 namespace TelCo.ColorCoder
 {
-    public class ColorCodeIdentifierTest
+    public class ColorCodePairHelperTests
     {
+        static ColorCodePairHelper colorCodePairHelper;
         private static void Main(string[] args)
         {
-            ColorCodeIdentifier colorCodeIdentifier = new ColorCodeIdentifier();
+            colorCodePairHelper = new ColorCodePairHelper();
+            ExecuteColorFromPairNumberTests();
+            ExecutePairNumberFromColorTests();
+        }
+        private static void ExecuteColorFromPairNumberTests()
+        {
             int pairNumber = 4;
-            ColorCode testPair1 = colorCodeIdentifier.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}", pairNumber, testPair1);
-            Debug.Assert(testPair1.MajorColor == Color.White);
-            Debug.Assert(testPair1.MinorColor == Color.Brown);
-
+            ColorCode testPair1 = colorCodePairHelper.GetColorCodeFromPairNumber(pairNumber);
+            Console.WriteLine(GetFormattedMessage( pairNumber, testPair1));
+            Debug.Assert(testPair1.MajorColor == Color.White && testPair1.MinorColor == Color.Brown);
             pairNumber = 5;
-            testPair1 = colorCodeIdentifier.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}", pairNumber, testPair1);
-            Debug.Assert(testPair1.MajorColor == Color.White);
-            Debug.Assert(testPair1.MinorColor == Color.SlateGray);
-
+            testPair1 = colorCodePairHelper.GetColorCodeFromPairNumber(pairNumber);
+            Console.WriteLine(GetFormattedMessage(pairNumber, testPair1));
+            Debug.Assert(testPair1.MajorColor == Color.White && testPair1.MinorColor == Color.SlateGray);
             pairNumber = 23;
-            testPair1 = colorCodeIdentifier.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}", pairNumber, testPair1);
-            Debug.Assert(testPair1.MajorColor == Color.Violet);
-            Debug.Assert(testPair1.MinorColor == Color.Green);
-
+            testPair1 = colorCodePairHelper.GetColorCodeFromPairNumber(pairNumber);
+            Console.WriteLine(GetFormattedMessage(pairNumber, testPair1));
+            Debug.Assert(testPair1.MajorColor == Color.Violet && testPair1.MinorColor == Color.Green);
+        }
+        private static void ExecutePairNumberFromColorTests()
+        {
             ColorCode testPair2 = new ColorCode() { MajorColor = Color.Yellow, MinorColor = Color.Green };
-            pairNumber = colorCodeIdentifier.GetPairNumberFromColor(testPair2);
-            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
+            int pairNumber = colorCodePairHelper.GetPairNumberFromColorCode(testPair2);
+            Console.WriteLine(GetFormattedMessage(testPair2, pairNumber));
             Debug.Assert(pairNumber == 18);
-
             testPair2 = new ColorCode() { MajorColor = Color.Red, MinorColor = Color.Blue };
-            pairNumber = colorCodeIdentifier.GetPairNumberFromColor(testPair2);
-            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
+            pairNumber = colorCodePairHelper.GetPairNumberFromColorCode(testPair2);
+            Console.WriteLine(GetFormattedMessage(testPair2, pairNumber));
             Debug.Assert(pairNumber == 6);
+        }
+        private static string GetFormattedMessage(int pairNumber,ColorCode colorCodePair)
+        {
+            return string.Format("[In]Pair Number: {0},[Out] Colors: {1}", pairNumber, colorCodePair);
+        }
+        private static string GetFormattedMessage(ColorCode colorCodePair, int pairNumber)
+        {
+            return string.Format("[In]Colors: {0}, [Out] PairNumber: {1}", colorCodePair, pairNumber);
         }
     }
 }
--- TelCo.ColorCoder/ColorCodePairHelper.cs
+++ TelCo.ColorCoder/ColorCodePairHelper.cs
@@ -4,18 +4,18 @@ using System.Text;
 using System.Linq;
 namespace TelCo.ColorCoder
 {
-    public class ColorCodeIdentifier
+    public class ColorCodePairHelper
     {
-        private Dictionary<int, ColorCode> ColorCodeReferences { get; set; }
-        public ColorCodeIdentifier()
+        private Dictionary<int, ColorCode> colorCodeReferences { get; set; }
+        public ColorCodePairHelper()
         {
-            ColorCodeReferences = new Dictionary<int, ColorCode>();
+            colorCodeReferences = new Dictionary<int, ColorCode>();
             int pairNumber = 1;
             for (int i = 0; i < ColorCode.ColorMapMajor.Length; i++)
             {
                 for (int j = 0; j < ColorCode.ColorMapMinor.Length; j++)
                 {
-                    ColorCodeReferences.Add(pairNumber, new ColorCode() { MajorColor = ColorCode.ColorMapMajor[i], MinorColor = ColorCode.ColorMapMinor[j] });
+                    colorCodeReferences.Add(pairNumber, new ColorCode() { MajorColor = ColorCode.ColorMapMajor[i], MinorColor = ColorCode.ColorMapMinor[j] });
                     pairNumber++;
                 }
             }
@@ -24,23 +24,23 @@ namespace TelCo.ColorCoder
         {
             StringBuilder colorCodeReferenceManual = new StringBuilder();
             colorCodeReferenceManual.AppendLine("PairNumber : Major Color - Minor Color");
-            foreach (KeyValuePair<int, ColorCode> colorCodeReference in ColorCodeReferences)
+            foreach (KeyValuePair<int, ColorCode> colorCodeReference in colorCodeReferences)
             {
                 colorCodeReferenceManual.AppendLine(string.Format("{0} : {1} - {2}", colorCodeReference.Key, colorCodeReference.Value.MajorColor, colorCodeReference.Value.MinorColor));
             }
             return colorCodeReferenceManual.ToString();
         }
-        internal ColorCode GetColorFromPairNumber(int pairNumber)
+        internal ColorCode GetColorCodeFromPairNumber(int pairNumber)
         {
-            if (!ColorCodeReferences.ContainsKey(pairNumber))
+            if (!colorCodeReferences.ContainsKey(pairNumber))
                 throw new ArgumentOutOfRangeException(string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-            return ColorCodeReferences[pairNumber];
+            return colorCodeReferences[pairNumber];
         }
-        internal int GetPairNumberFromColor(ColorCode pair)
+        internal int GetPairNumberFromColorCode(ColorCode pair)
         {
-            if (ColorCodeReferences.Where(x => x.Value.MajorColor.Equals(pair.MajorColor) && x.Value.MinorColor.Equals(pair.MinorColor)).Count() == 0)
+            if (colorCodeReferences.Where(x => x.Value.MajorColor.Equals(pair.MajorColor) && x.Value.MinorColor.Equals(pair.MinorColor)).Count() == 0)
                 throw new ArgumentException(string.Format("Unknown Colors: {0}", pair.ToString()));
-            return ColorCodeReferences.FirstOrDefault(x => x.Value.MajorColor.Equals(pair.MajorColor) && x.Value.MinorColor.Equals(pair.MinorColor)).Key;
+            return colorCodeReferences.FirstOrDefault(x => x.Value.MajorColor.Equals(pair.MajorColor) && x.Value.MinorColor.Equals(pair.MinorColor)).Key;
         }
     }
 }
